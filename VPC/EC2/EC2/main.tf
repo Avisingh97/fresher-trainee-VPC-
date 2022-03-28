3fff3ba5814de0d9b927e71bec1648a3cc051f1a
Avisingh97 <78423303+Avisingh97@users.noreply.github.com>
2022-03-28 16:34:07 +0530

    Delete VPC directory

--- VPC/EC2/EC2/main.tf
+++
@@ -1,8 +0,0 @@
-resource"aws_instance" "Ec2"{
-    ami=var.ami
-    instance_type = var.instance_type
-    
-    tags={
-     Name = var.name
-    }
-}
--- VPC/EC2/EC2/output.tf
+++
@@ -1,3 +0,0 @@
-output"public-ip"{
-    value= aws_instance.Ec2.public_ip
-}
